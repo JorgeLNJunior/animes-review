7175466bd64b00a9fe11a3b3789dc460fa00d3a5
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-07-06 21:11:57 -0300

    chore: configurar o import alias para components

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@components': resolve(__dirname, '/src/components')
+    }
+  }
 })

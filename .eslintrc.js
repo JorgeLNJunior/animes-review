2b16a4103c0508a619fe63e774707a5586e4945d
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-07-06 21:11:02 -0300

    style: modificar as regras do eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,21 @@
+import { resolve } from 'path'
+
 module.exports = {
   env: {
     browser: true,
     es2021: true
   },
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
     'standard'
   ],
   parserOptions: {
-    ecmaVersion: 12,
+    extraFileExtensions: ['.vue'],
     parser: '@typescript-eslint/parser',
+    project: resolve(__dirname, './tsconfig.json'),
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2018,
     sourceType: 'module'
   },
   plugins: [

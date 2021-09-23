882f09956c5e61065511f685ed0c54dc317b5045
JorgeLNJunior <jorgelnjunior@gmail.com>
2021-09-23 15:04:22 -0300

    feat: criar base da admin home

--- src/pages/admin/AdminHome.vue
+++ src/pages/admin/AdminHome.vue
@@ -1,13 +1,50 @@
 <template>
-  <div>
-    <h1>Admin Home Page</h1>
+  <div class="columns is-centered mt-3 mr-2 ml-2">
+    <div class="column is-8">
+      <Chat />
+    </div>
+    <div class="column is-3">
+      <div class="card">
+        <div class="card-header">
+          <p class="card-header-title">
+            Top 10
+          </p>
+        </div>
+        <div class="card-content">
+          <TopItem
+            v-for="(anime, index) of state.top10"
+            :key="index"
+            :position="index + 1"
+            :anime="anime"
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { Anime, AnimeApi } from '@api/Anime'
+import Chat from '@components/home/chat/Chat.vue'
+import TopItem from '@components/home/TopItem.vue'
+import { defineComponent, onBeforeMount, reactive } from 'vue'
 
 export default defineComponent({
-  name: 'AdminHomePage'
+  name: 'AdminHomePage',
+  components: { TopItem, Chat },
+  setup () {
+    const animeApi = new AnimeApi()
+    const state = reactive({
+      top10: [] as Anime[]
+    })
+
+    onBeforeMount(async () => await findTop10())
+
+    async function findTop10 () {
+      state.top10 = await animeApi.findTop10()
+    }
+
+    return { state }
+  }
 })
 </script>
